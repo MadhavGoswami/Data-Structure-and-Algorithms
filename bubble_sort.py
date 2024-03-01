4f00e236d785984227b40220a30d8d9fcf9c5feb
Madhav <thegreatmadhav16@gmail.com>
2024-03-01 17:49:09 +0530

    Bubble_Sort File

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+arr=[7,3,5,8,9,2]
+n=len(arr)
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        if arr[j]>arr[j+1]:
+            temp=arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
+
+print(arr)
